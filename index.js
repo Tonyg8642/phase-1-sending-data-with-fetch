8e4d3b5d4a8eb9130458d5e1212275ca487d8053
Tony Glass <idttip.tg@gmail.com>
2023-11-07 17:59:00 -0800

    completed Sending Data With Fetch

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+
+  function submitData(UserName, UserEmail){
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+        name:  UserName,
+        email: UserEmail,
+        })
+    
+      };
+    
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then(function (response) {
+        return response.json();
+      }).then(function (Data){
+        document.body.append(Data.id)
+      }).catch(function (error){
+        alert(error.message)
+        document.body.append(error.message);
+      });
+      
+  }
